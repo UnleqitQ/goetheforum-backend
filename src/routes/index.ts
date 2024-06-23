0899f558cd80320135811cc910c3d9ec73a05ee5
UnleqitQ <quentin.weinbrenner@gmail.com>
2024-06-23 21:27:44 +0200

    add swagger route for api docs

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import {Router} from 'express';
 import account from './account';
+import swagger from './swagger';
 
 const router = Router();
 
 // Add sub-routes
 router.use('/account', account);
+router.use('/', swagger);
 
 export default router;
---
+++ src/routes/swagger.ts
@@ -0,0 +1,15 @@
+import {Router} from 'express';
+import swaggerUi from 'swagger-ui-express';
+import path from 'node:path';
+import fs from 'node:fs';
+import yaml from 'yaml';
+
+const filePath = path.resolve(process.cwd(), 'api.yaml');
+const file = fs.readFileSync(filePath, 'utf-8');
+const swaggerDocument = yaml.parse(file);
+
+const router = Router();
+
+router.use('/swagger-ui', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+export default router;
