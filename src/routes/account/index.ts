bdee28ae6e649c0e490b17d83517517b80279560
UnleqitQ <quentin.weinbrenner@gmail.com>
2024-06-23 20:50:17 +0200

    add change password route

--- src/routes/account/index.ts
+++ src/routes/account/index.ts
@@ -2,11 +2,13 @@ import {Router} from 'express';
 import register from './register';
 import login from './login';
 import refresh from './refresh';
+import change_password from './change_password';
 
 const router = Router();
 
 router.use('/register', register);
 router.use('/login', login);
 router.use('/refresh', refresh);
+router.use('/change_password', change_password);
 
 export default router;
